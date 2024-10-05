274db9878ddd36c5e51175023a2d4506cbb4b429
evanfp582 <evanfp582@gmail.com>
2024-10-05 17:49:25 -0400

    Get all drink/ingredients implemented

--- client/src/pages/OrderView.jsx
+++ client/src/pages/OrderView.jsx
@@ -1,41 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 import Radio from '@mui/material/Radio';
+import axios from 'axios';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+const drinkDataModel = [
+  {name: ""}
+] 
 
 const OrderView= () => {
-  const [drinkQueue, setDrinkQueue] = useState([])
   const [drinkList, setDrinkList] = useState([])
   const [name, setName] = useState("")
   const [drink, setDrink] = useState("")
 
   useEffect(() => {
-    setDrinkQueue(exampleDataJSON.orders)
-  }, [drinkQueue]);
-
-  useEffect(() => {
-    setDrinkList(exampleDrinkList.drinks)
-  }, [drinkList]);
+    // Gets all drinks and sets it to dronkList
+    axios.get("http://localhost:5000/cocktails")
+      .then(response => {
+        setDrinkList(response.data.map(drink => drink.name))
+      })
+      .catch(error => {
+        console.log("Error fetching data: ", error);
+        
+      })
+  }, []);
 
   const enterName = (e) => {
-    const { value } = e.target;
-    setName(value);
+    // const { value } = e.target;
+    // setName(value);
   };
 
 
@@ -44,15 +41,6 @@ const OrderView= () => {
     // Perform Submit logic here with drinkData
     console.log('name:', name);
     console.log('drink:', drink);
-    // try {
-    //   axios.post("/CreateDrinkFormSubmit", {drinkData})
-    //     .then(res => {
-    //       console.log(res.data);
-    //       resetDrinkData()
-    //     })
-    // } catch (error) {
-    //   console.error(error.message);
-    // }
 
   };
 
@@ -92,7 +80,7 @@ const OrderView= () => {
               required
             >
               {drinkList.map((drink, index) => (
-                <FormControlLabel value={drink.drink} control={<Radio required={true} />} label={drink.drink} />
+                <FormControlLabel key={index} value={drink} control={<Radio/>} label={drink} />
               ))}   
             </RadioGroup>
           </FormControl>
@@ -108,50 +96,5 @@ const OrderView= () => {
   </>
 
 }
-
-let exampleDrinkList= {
-  "drinks":[
-    {
-      "drink": "mojito",
-      "subtext": "minty fresh rum"
-    },
-    {
-      "drink": "whiskey sour",
-      "subtext": "Drink whiskey, but its tasty"
-    },
-    {
-      "drink": "manhatan",
-      "subtext": "put some hair on your chest"
-    },
-    {
-      "drink": "gin fizz",
-      "subtext": "hurt my arm"
-    },
-    {
-      "drink": "marg",
-      "subtext": "tequilla and lime"
-    },
-    {
-      "drink": "vodka cran",
-      "subtext": "vodka + cran"
-  },
-  ]
-}
-
-let exampleDataJSON = {
-  "orders":[
-     {
-        "drink":"mojito",
-        "name":"Gilbert",
-        "time":"7:50"
-     },
-     {
-        "drink":"whiskey sour",
-        "name":"Zel",
-        "time":"7:51"
-     }
-    ]
-}
-
 
 export default OrderView
